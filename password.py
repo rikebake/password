06b7caa343951f4128f631335ed5f46176e495db
Richard Tsai <rikebake@mail2000.com.tw>
2022-12-27 11:33:32 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,13 @@
+password = 'a123456' #real password
+trial = 3 # the # of trial left for users to input the password / maximum trial = 3 
+while True:
+	user_input = input('input the password ')
+	trial = trial - 1
+	if user_input == password:
+		print('Welcome!!')
+		break
+	elif trial > 0:
+		print('incorrect,you got ',trial,' times left')
+	elif trial == 0:
+		print('Oops, no chance. You have tried 3 times')   
+		break
